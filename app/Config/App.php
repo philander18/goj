1bf5aa0ebf35ec73a73b3a58c9b949332e7889b7
philander18 <philander.totalis@gmail.com>
2023-10-16 15:36:16 +0700

    edit file app terkait base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://goj-encounter/';
+    public string $baseURL = 'https://goj-encounter.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
